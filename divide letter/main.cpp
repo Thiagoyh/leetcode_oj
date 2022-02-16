e42110eb368383ded23db6fe9938e01a8a92e532
Thiagoyh <lwisyh@gmail.com>
2022-02-16 21:24:44 +0800

    这个划分字母，绝对是map

---
+++ divide letter/main.cpp
@@ -0,0 +1,34 @@
+#include "bits/stdc++.h"
+
+int main(){
+
+    // input
+    std::string s;
+    char ch;
+    while (std::cin >> s){
+        if((ch = getchar()) == '\n')
+            break;
+    }
+
+    //algorithm
+    std::vector<int> res;
+    std::unordered_map<char, int> map;
+
+    int start = 0, end = 0;
+    for(int i = 0; i != s.size(); ++i){
+        map[s[i]] = i;
+    }
+
+    for (int i = 0; i != s.size(); ++i){
+        end = std::max(end, map[s[i]]);
+        if(i == end){
+            res.push_back(end - start + 1);
+            start = end + 1;
+        }
+    }
+    for (int i = 0; i != res.size(); ++i){
+        std::cout << res[i] << std::endl;
+    }
+
+    return 0;
+}
