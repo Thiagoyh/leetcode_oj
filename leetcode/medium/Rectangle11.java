905a922a64d09cdba556b6930d67e7c58bb762b0
Thiagoyh <lwisyh@gmail.com>
2022-03-24 22:14:05 +0800

    预处理牛逼

--- leetcode/medium/Rectangle11.java
+++ leetcode/medium/Rectangle11.java
@@ -32,4 +32,74 @@ public class Rectangle11 {
         }
         return res;
     }
+
+    // a better solution
+    // record the number of arranged continuously 1111
+    public static int maxOneMatrix(int[][] mat){
+        int N = mat.length;
+        int numR = 0, numD = 0;
+        int[][] numMatR = new int[N][N];
+        int[][] numMatD = new int[N][N];
+        for(int i = N - 1; i >= 0; i--){
+            for(int j = N - 1; j >= 0; j--){
+                if(mat[i][j] == 1){
+                    numR++;
+                    numMatR[i][j] = numR;
+                }
+                else{
+                    numMatR[i][j] = 0;
+                    numR = 0;
+                }
+            }
+            numR = 0;
+        }
+        for(int j = N - 1; j >= 0; j--){
+            for(int i = N - 1; i >= 0; i--){
+                if(mat[i][j] == 1){
+                    numD++;
+                    numMatD[i][j] = numD;
+                }
+                else{
+                    numMatD[i][j] = 0;
+                    numD = 0;
+                }
+            }
+            numD = 0;
+        }
+        int res = 0;
+        for(int i = 0; i < N; ++i){
+            for(int j = 0; j < N; ++j){
+                if(numMatD[i][j] == 0 || numMatR[i][j] == 0){
+                    continue;
+                }
+                if(Math.min(numMatD[i][j], numMatR[i][j]) == 1){
+                    res = res < 1 ? 1 : res;
+                }
+
+                int k = Math.min(numMatD[i][j], numMatR[i][j]);
+                int downNum = numMatR[i + k - 1][j];
+                int rightNum = numMatD[i][j + k - 1];
+                int tmp = Math.min(Math.min(k, downNum), rightNum);
+                res = tmp > res ? tmp : res;
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[][] mat = {{0, 1, 1, 1, 1},
+                       {0, 1, 0, 0, 1},
+                       {0, 1, 0, 0, 1},
+                       {0, 1, 1, 1, 1},
+                       {0, 1, 0, 1, 1}};
+        System.out.println(maxOneMatrix(mat));
+    }
 }
+
+/*
+ * 0 1 1 1 1
+ * 0 1 0 0 1
+ * 0 1 0 0 1
+ * 0 1 1 1 1
+ * 0 1 0 1 1
+ */
