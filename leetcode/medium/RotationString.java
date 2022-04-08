ea742f3e207434b7536d9e373688696cb6fa7fa3
Thiagoyh <lwisyh@gmail.com>
2022-04-08 15:23:30 +0800

    kmp和咖啡杯问题

--- leetcode/medium/RotationString.java
+++ leetcode/medium/RotationString.java
@@ -10,10 +10,66 @@ package com.lewis.leetcode.medium;
 // a字符串扩展成两倍之后, 看b是不是a的子串
 
 public class RotationString {
-     public static boolean isRotation(String a, String b){
-         char[] strA = a.toCharArray();
-         char[] strB = b.toCharArray();
 
-         return true;
+    public static int getIndex(String str1, String str2){
+        if(str1 == null || str1 == null || str1.length() < 1 ||
+                str1.length() < str1.length()){
+            return -1;
+        }
+        char[] strA = str1.toCharArray();
+        char[] strB = str2.toCharArray();
+        int i1 = 0, i2 = 0;
+        int[] next = getNextArray(strB);
+        while(i1 < strA.length && i2 < strB.length){
+            if(strA[i1] == strB[i2]){
+                i1++;
+                i2++;
+            }
+            else if(i2 == 0){
+                i1++;
+            }
+            else {
+                i2 = next[i2];
+            }
+        }
+        return i2 == strB.length ? i1 - i2 : -1;
+    }
+    public static int[] getNextArray(char[] str2){
+        if(str2.length == 1){
+            return new int[] {-1};
+        }
+        int[] next = new int[str2.length];
+        next[0] = -1;
+        next[1] = 0;
+        int i = 2;
+        int cn = 0;
+
+        while(i < str2.length){
+            if(str2[i - 1] == str2[cn]){
+                next[i++] = ++cn;
+            }
+            else if (cn > 0){
+                cn = next[cn];
+            }
+            else{
+                next[i++] = 0;;
+            }
+        }
+        return next;
+    }
+
+     public static boolean isRotation(String a, String b){
+         String a2 = a + a;
+         int index = getIndex(a2, b);
+         if(index >= 0)
+             return true;
+         else
+             return false;
      }
+
+    public static void main(String[] args) {
+        String str1 = "12345";
+        String str2 = "45123";
+        System.out.println(isRotation(str1, str2));
+    }
 }
