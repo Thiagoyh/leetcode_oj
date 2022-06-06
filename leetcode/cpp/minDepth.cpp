f7025902ecbc07caa383509d0ba479144bfdd24a
Thiagoyh <lwisyh@gmail.com>
2022-06-06 10:55:08 +0800

    最新深度递归

--- leetcode/cpp/minDepth.cpp
+++ leetcode/cpp/minDepth.cpp
@@ -19,22 +19,19 @@ public:
          return process(root);
     }
     int process(TreeNode* root) {
-        if (root->left == nullptr && root->right == nullptr) {
+        if (root == nullptr) {
             return 0;
-        }
-        int min = __INT32_MAX__;
-        if (root->left == nullptr) {
-            int depth = process(root->right);
-            min = depth < min ? depth : min;
-            return 1 + min;
-        }
-        if (root->right == nullptr) {
-            int depth = process(root->left);
-            min = depth < min ? depth : min;
-            return 1 + min;
         }
         int left = process(root->left);
         int right = process(root->right);
-        return 1 + std::min(left, right);
+
+        if (root->left == nullptr && root->right != nullptr) {
+            return 1 + right;
+        }
+        if (root->left != nullptr && root->right == nullptr) {
+            return 1 + left;
+        }
+
+        return (1 + std::min(left, right));
     }
 };
