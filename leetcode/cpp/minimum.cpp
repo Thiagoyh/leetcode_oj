2a105aab1d6105202a46f50a4f48bf78241e26cb
Thiagoyh <lwisyh@gmail.com>
2022-06-10 22:54:18 +0800

    今天冲

--- leetcode/cpp/minimum.cpp
+++ leetcode/cpp/minimum.cpp
@@ -1,4 +1,5 @@
-
+#include <algorithm>
+#include <limits.h>
 struct TreeNode {
     int val;
     TreeNode* left;
@@ -9,9 +10,29 @@ struct TreeNode {
              val(val), left(left), right(right) {}
 };
 
+struct Info {
+    int max;
+    int min;
+};
+
 class Solution {
 public:
+    int res = INT_MAX;
+    TreeNode* pre;
     int getMinimumDifference(TreeNode* root) {
+        process(root);
+        return res;
+    }
 
+    void process(TreeNode* root) {
+        if (root == nullptr) {
+            return;
+        }
+        process(root->left);
+        if (pre != nullptr) {
+            res = (root->val - pre->val) < res ? (root->val - pre->val) : res;
+        }
+        pre = root;
+        process(root->right);
     }
 };
