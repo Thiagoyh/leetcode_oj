9f8089a5d02a85b4e97d118fca7a61965e78b1d7
Thiagoyh <lwisyh@gmail.com>
2022-06-12 15:17:43 +0800

    这个就递归

---
+++ leetcode/cpp/commonAncestor.cpp
@@ -0,0 +1,34 @@
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int val) : val(val), left(nullptr), right(nullptr) {}
+    TreeNode(int val, TreeNode* left, TreeNode* right) :
+             val(val), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (root == p || root == q || root == nullptr) {
+            return root;
+        }
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+
+        if (right != nullptr && left != nullptr) {
+            return root;
+        }
+        if (right == nullptr && left != nullptr) {
+            return left;
+        }
+        else if (left == nullptr && right != nullptr) {
+            return right;
+        }
+        else {
+            return nullptr;
+        }
+    }
+};
