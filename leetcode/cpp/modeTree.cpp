40f4654c7e23253186e694e500dce11cdc5c1246
Thiagoyh <lwisyh@gmail.com>
2022-06-12 10:59:07 +0800

    使用优先级队列

--- leetcode/cpp/modeTree.cpp
+++ leetcode/cpp/modeTree.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <queue>
 
 using namespace std;
 
@@ -12,19 +13,55 @@ struct TreeNode {
              val(val), left(left), right(right) {}
 };
 
+struct temp {
+    bool operator()(std::pair<int, int> a, std::pair<int, int> b) {
+        return a.second < b.second;
+    }
+};
 class Solution {
 public:
+    int pre = (1 << 6);
     vector<int> findMode(TreeNode* root) {
+        std::vector<int> res;
+        if (root == nullptr) {
+            return res;
+        }
         std::vector<int> nums;
         process(root, nums);
-
+        std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, temp> queue;
+        int pre = nums[0];
+        int count = 1;
+        for (int i = 1; i < nums.size(); ++i) {
+            if (nums[i] == pre) {
+                count++;
+            }
+            else {
+                queue.push(std::pair<int, int>(pre, count));
+                count = 1;
+                pre = nums[i];
+            }
+        }
+        queue.push(std::pair<int, int>(nums[nums.size() - 1], count));
+        pre = queue.top().second;
+        res.push_back(queue.top().first);
+        queue.pop();
+        while (!queue.empty()) {
+            if (queue.top().second == pre) {
+                res.push_back(queue.top().first);
+                queue.pop();
+            }
+            else {
+                break;
+            }
+        }
+        return res;
     }
-    void process(TreeNode* root, std::vector<int>& res) {
+    void process(TreeNode* root, std::vector<int>& nums) {
         if (root == nullptr) {
             return;
         }
-        process(root->left, res);
-        res.push_back(root->val);
-        process(root->right, res);
+        process(root->left, nums);
+        nums.push_back(root->val);
+        process(root->right, nums);
     }
 };
