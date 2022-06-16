df645b574f45ddb04349be2f657c700c24fd725f
Thiagoyh <lwisyh@gmail.com>
2022-06-17 00:24:55 +0800

    5%

---
+++ leetcode/cpp/traceback/reconstructIterary.cpp
@@ -0,0 +1,73 @@
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<string> findItinerary(vector<vector<string>>& tickets) {
+        std::vector<std::string> res;
+        if (tickets.size() == 0) {
+            return res;
+        }
+        std::vector<std::string> path;
+        std::unordered_map<std::string, std::vector<string>> map;
+        std::unordered_map<std::string, int> used_set;
+        for (int i = 0; i < tickets.size(); i++) {
+            map[tickets[i][0]].push_back(tickets[i][1]);
+            used_set[tickets[i][0] + "." + tickets[i][1]]++;
+        }
+        for (auto it = map.begin(); it != map.end(); ++it) {
+            std::sort(it->second.begin(), it->second.end());
+        }
+        for (int i = 0; i < map["JFK"].size(); ++i) {
+            path.push_back("JFK");
+            path.push_back(map["JFK"][i]);
+            used_set["JFK." + map["JFK"][i]]--;
+            process(res, path, tickets, map, used_set, map["JFK"][i]);
+            used_set["JFK." + map["JFK"][i]]++;
+            path.pop_back();
+            path.pop_back();
+        }
+        return res;
+    }
+    void process(std::vector<std::string>& res,
+                 std::vector<std::string>& path,
+                 std::vector<std::vector<std::string>>& tickets,
+                 std::unordered_map<std::string, std::vector<string>>& map,
+                 std::unordered_map<std::string, int>& used_set,
+                 std::string& target) {
+        if (path.size() == tickets.size() + 1) {
+            res = path;
+            return;
+        }
+        for (int i = 0; i < map[target].size(); i++) {
+            if (used_set[target + "." + map[target][i]] == 0) {
+                continue;
+            }
+            if (res.size() != 0 && !compare(res[path.size()], map[target][i])) {
+                continue;
+            }
+            path.push_back(map[target][i]);
+            used_set[target + "." + map[target][i]]--;
+            process(res, path, tickets, map, used_set, map[target][i]);
+            used_set[target + "." + map[target][i]]++;
+            path.pop_back();
+        }
+
+    }
+
+    bool compare(std::string& in_res, std::string& in_path) {
+        for (int i = 0; i < in_path.size() && i < in_res.size(); ++i) {
+            if (in_path[i] < in_res[i]) {
+                return true;
+            }
+            if (in_path[i] > in_res[i]) {
+                return false;
+            }
+        }
+        return in_path.size() < in_res.size();
+    }
+};
