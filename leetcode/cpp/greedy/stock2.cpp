adc48c79acca52e74f6836a17117ef96d93fd4fc
Thiagoyh <lwisyh@gmail.com>
2022-06-22 23:11:29 +0800

    这个多少有点难操作了

---
+++ leetcode/cpp/greedy/stock2.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices, int fee) {
+        int res = 0;
+        int min = prices[0];
+        for (int i = 1; i < prices.size(); ++i) {
+            if (prices[i] < min) {
+                min = prices[i];
+            }
+            if (prices[i] - min - fee < 0 && prices[i] > min) {
+                continue;
+            }
+            if (prices[i] > min + fee) {
+                res += prices[i] - min - fee;
+                min = prices[i] - fee;
+            }
+        }
+    }
+};
